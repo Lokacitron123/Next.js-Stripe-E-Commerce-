90dfcb02d18c8e6fdee8b59c153c30f03e38d31b
Johan Lindell <johan.lindell91@gmail.com>
2024-01-08 15:25:50 +0100

    Adds header component with shoppingcart icon. keeps check of X amount of products in cart

---
+++ project/src/components/Header/Header.tsx
@@ -0,0 +1,48 @@
+import { redirect } from "next/navigation";
+import Link from "next/link";
+import ShoppingCartBtn from "./ShoppingCartBtn";
+import { getCart } from "@/actions/actions";
+
+const searchProducts = async (formData: FormData) => {
+  "use server";
+
+  const searchQuery = formData.get("searchQuery")?.toString();
+
+  if (searchQuery) {
+    redirect("/search?query=" + searchQuery);
+  }
+};
+
+const Header = async () => {
+  const cart = await getCart();
+
+  return (
+    <header className='bg-base-100'>
+      <nav className='navbar max-w-7xl m-auto flex-col sm:flex-row gap-2'>
+        <div className=' '>
+          <Link href={"/"} className='font-bold btn btn-ghost text-xl'>
+            E-SHOP LOGO
+          </Link>
+        </div>
+
+        <ul className='flex-none gap-2'>
+          <Link href={"/products"}>Produkter</Link>
+        </ul>
+
+        <form action={searchProducts}>
+          <div className='form-control'>
+            <input
+              name='searchQuery'
+              placeholder='sök'
+              className='input input-bordered w-full min-w-[200px]'
+            />
+          </div>
+        </form>
+
+        <ShoppingCartBtn cart={cart} />
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
